9978fb1c607bf2a4c39a927492caaab2dfaa2be3
hmustain <hunter@bunkerbranding.com>
2022-11-08 12:08:04 -0600

    saving work, trying to make a function to make time dynamic, going back to askbcs

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,6 +5,11 @@ var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#current-time").text(time);
 
 //need time to update dynamically instead of on page refresh
+function currentTime() {
+    time = setInterval("#current-time", 1000);
+}
+
+currentTime();
 
 
 
