1ea496775d559f0b4c6106717de5ff8b236ab6e2
hmustain <hunter@bunkerbranding.com>
2022-11-08 09:31:49 -0600

    added js file linked to html, added current date and time to index.html using moment.js

---
+++ assets/scripts.js
@@ -0,0 +1,5 @@
+// going to use moment.js to add the current date and time to the top of the page
+// https://momentjs.com/docs/#/displaying/
+// went back to activity 25 and 26 for reference
+var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+$("#current-time").text(time);
