1b704cbc73f217de637f1d1a3423b59854991767
hmustain <hunter@bunkerbranding.com>
2022-11-08 12:22:31 -0600

    added setInterval function so time would display dynamically instead of static

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,11 +5,11 @@ var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#current-time").text(time);
 
 //need time to update dynamically instead of on page refresh
-function currentTime() {
-    time = setInterval("#current-time", 1000);
-}
-
-currentTime();
+// asked bcs helped me here
+setInterval(function(){
+    var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    $("#current-time").text(time);
+    }, 1000);
 
 
 
